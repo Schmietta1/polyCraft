17ad1218f4322d8b2febc46ff8b8220d2e805c48
Grégoire PAUL <89679258+Aoxcis@users.noreply.github.com>
2024-11-18 20:16:56 +0100

    non fonctionnel

--- src/main/java/name/modid/ModBlocks.java
+++ src/main/java/name/modid/ModBlocks.java
@@ -2,45 +2,44 @@ package name.modid;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.component.type.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
-
-
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.block.AbstractBlock.Settings;
+import static net.minecraft.block.AbstractBlock.Settings.*;
 
 public class ModBlocks {
     // This is a class that contains all of our items
     private ModBlocks() {
     }
 
-    // list to keep track of our items
+    // list to keep track of our blocks
     public static final List<Block> BLOCKS = new ArrayList<>();
 
-    // create items
-    public static final Block CONDENSED_DIRT = register(
-        "condensed_dirt",
-        settings -> new Block(settings.sounds(BlockSoundGroup.GRASS)),
-        AbstractBlock.Settings.create()
-);
+    // create blocks
+    public static final Block CUSTOM_BLOCK = register(
+            "stone",
+            new Block(Settings.create().mapColor(MapColor.STONE_GRAY).strength(1.5F, 6.0F))
+    );
 
-    public static Block register(String path, Function<Block.Settings, Block> factory, Block.Settings settings) {
+    public static Block register(String path, Block block) {
         final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of("polycraft", path));
 
-        Block block = Blocks.register(registryKey, factory, settings);
-        BLOCKS.add(block);
-        
-        return block;
+        Block block2 = Registry.register(Registries.BLOCK, registryKey.getValue(), block);
+        BLOCKS.add(block2);
+
+        return block2;
     }
 
     public static void initialize() {
@@ -48,7 +47,7 @@ public class ModBlocks {
         // but we will do that in the register method
         ItemGroupEvents.modifyEntriesEvent(ModItemGroup.CUSTOM_ITEM_GROUP_KEY).register(entries -> {
             for (Block block : BLOCKS) {
-                entries.add(ModBlocks.CONDENSED_DIRT.asItem());
+                entries.add(block.asItem());
             }
         });
     }
